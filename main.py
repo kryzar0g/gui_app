a0e943685dd8398a42ad064498631960f2e40103
kryzar0g <97110766+kryzar0g@users.noreply.github.com>
2023-06-10 00:41:21 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,10 @@ import pyqtgraph as pg
 from PyQt5.QtSerialPort import QSerialPort, QSerialPortInfo
 from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QHBoxLayout,QPushButton, QLineEdit
 from PyQt5.QtGui import QPalette, QColor
-from PyQt5.QtCore import QIODevice,QTimer
+from PyQt5.QtCore import QIODevice
 import time
 from PyQt5.QtWidgets import QFileDialog
+from PyQt5 import QtCore
 
 class Color(QWidget):
 
@@ -21,13 +22,65 @@ class Color(QWidget):
 
 class MainWindow(QMainWindow):
 
+    def update_plot_data(self):
+        if self.typ_grafu.currentText() == "ppm":
+            self.x = self.x[1:]  # Remove the first element
+            self.x.append(self.x[-1] + 1)  # Add a new value 1 higher than the last
+            self.y = self.y[1:]  # Remove the first element
+            self.y.append(self.ppm)  # Add a new data point
+            self.data_line.setData(self.x, self.y)
+
+        if self.typ_grafu.currentText() == "temperature":
+            self.x = self.x[1:]
+            self.x.append(self.x[-1] + 1)
+            self.y = self.y[1:]
+            self.y.append(self.temp)
+            self.data_line.setData(self.x, self.y)
+
+        if self.typ_grafu.currentText() == "humidity":
+            self.x = self.x[1:]
+            self.x.append(self.x[-1] + 1)
+            self.y = self.y[1:]
+            self.y.append(self.hum)
+            self.data_line.setData(self.x, self.y)
+
+
+
+    def cteni_dat(self):
+        data = self.port.readAll().data().decode('utf-8').strip()
+        self.serialData.append(data)
+        self.serialDataHex.append(data.encode('utf-8').hex())
+        print(data)
+
+        hodnoty = []  # Initialize hodnoty as an empty list
+
+        if data:
+            hodnoty = data.split(";")
+
+        if len(hodnoty) == 3:
+            ppm = float(hodnoty[0])
+            temp = float(hodnoty[1])
+            hum = float(hodnoty[2].split("\r\n")[0])  # Extract numeric part and convert to float
+            print("ppm: ", ppm)
+            print("temp: ", temp)
+            print("hum: ", hum)
+
+            self.ppm = ppm
+            self.temp = temp
+            self.hum = hum
+            # Update ppm value
+
+            self.update_plot_data()  # Call update_plot_data to update the plot
+
+
+
     def __init__(self):
         super(MainWindow, self).__init__()
 
 
 
         self.setWindowTitle("co2 monitoring")
-        self.setGeometry(0, 0, 1500, 900)
+
 
         self.port = QSerialPort()
 
@@ -39,6 +92,9 @@ class MainWindow(QMainWindow):
         self.portNames.currentIndexChanged.connect(self.index_changed)
         self.portNames.currentTextChanged.connect(self.index_changed)
 
+        self.ppm=0
+        self.temp=0
+        self.hum=0
 
         # baudrate
         self.baudRates = QtWidgets.QComboBox(self)
@@ -50,12 +106,6 @@ class MainWindow(QMainWindow):
         self.baudRates.currentIndexChanged.connect(self.index_changed)
         self.baudRates.currentTextChanged.connect(self.index_changed)
 
-        self.typ_sensoru = QtWidgets.QComboBox(self)
-        self.typ_sensoru.addItems(['MH-Z19B', 'Sensirion SCD40'])
-        self.typ_sensoru.currentIndexChanged.connect(self.index_changed)
-        self.typ_sensoru.currentTextChanged.connect(self.index_changed)
-
-
         self.button_is_checked = False
         self.button = QPushButton("Connect")
         self.button.setFixedSize(250, 100)
@@ -63,52 +113,42 @@ class MainWindow(QMainWindow):
         self.button.released.connect(self.the_button_was_released)
         self.button.setChecked(self.button_is_checked)
 
+        self.typ_grafu = QtWidgets.QComboBox(self)
+        self.typ_grafu.addItems(['ppm', 'temperature', 'humidity'])
+        self.typ_grafu.currentTextChanged.connect(self.index_changed)
+        self.typ_grafu.currentIndexChanged.connect(self.index_changed)
+
+
+
         self.graf = pg.PlotWidget()
-        self.graf.setFixedSize(1300, 500)
+        self.setCentralWidget(self.graf)
+        self.graf.showGrid(x=True, y=True)
+        self.x = list(range(1000))  # 100 time points
+        self.y = [0] * 1000  # 100 data points
+
         self.graf.setBackground('w')
 
-        self.x = list(range(100))  # 100 time points
-        self.y_ppm = [0] * 100  # Initialize data points for ppm
+        pen = pg.mkPen(color=(215, 51, 112))
+        self.data_line = self.graf.plot(self.x, self.y, pen=pen)
 
-        pen_ppm = pg.mkPen(color=(255, 0, 0))
-
-        self.data_line_ppm = self.graf.plot(self.x, self.y_ppm, pen=pen_ppm)
-
-        self.timer = QTimer()
-        self.timer.setInterval(200)
+        self.timer = QtCore.QTimer()
+        self.timer.setInterval(10)
         self.timer.timeout.connect(self.update_plot_data)
         self.timer.start()
 
-
-        input = QLineEdit()
-        input.setStyleSheet("background-color: white")
-        font = input.font()
-        font.setPointSize(10)
-        input.setFont(font)
-        input.setMaxLength(150)
-        input.setPlaceholderText("Enter your text")
-        input.setFixedSize(400, 400)
-        # input.setInputMask('00.00.00.00.00.00.00.00.00.00.00.00;_')
-        input.returnPressed.connect(self.return_pressed)
-        input.selectionChanged.connect(self.selection_changed)
-        input.textChanged.connect(self.index_changed)
-        input.textEdited.connect(self.text_edited)
-
-
         self.serialData = QtWidgets.QTextEdit(self)
         self.serialData.setReadOnly(True)
-        self.serialData.setFontFamily('Comic Sans MS')
+        self.serialData.setFontFamily('Calibri')
         self.serialData.setSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
 
         self.serialDataHex = QtWidgets.QTextEdit(self)
         self.serialDataHex.setReadOnly(True)
-        self.serialDataHex.setFontFamily('Comic Sans MS')
-        self.serialDataHex.setFixedWidth(400)
+        self.serialDataHex.setFontFamily('Calibri')
         self.serialDataHex.setSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
 
         self.fileNameInput = QLineEdit()
         self.fileNameInput.setStyleSheet("background-color: white")
-        self.fileNameInput.setPlaceholderText("soubor pro ulozeni bez pripony")
+        self.fileNameInput.setPlaceholderText("nazev souboru k ulozeni ve formatu .csv")
         self.fileNameInput.returnPressed.connect(self.save_file)
 
         # normalni layout ..
@@ -122,7 +162,7 @@ class MainWindow(QMainWindow):
 
         layout2.addWidget(self.portNames)
         layout2.addWidget(self.baudRates)
-        layout2.addWidget(self.typ_sensoru)
+        layout2.addWidget(self.typ_grafu)
         layout2.addWidget(self.fileNameInput)
         layout2.addWidget(self.button)
 
@@ -132,7 +172,6 @@ class MainWindow(QMainWindow):
         layout3.addWidget(self.graf)
 
         layout3.addLayout(layout4)
-        layout4.addWidget(input)
         layout4.addWidget(self.serialData)
         layout4.addWidget(self.serialDataHex)
 
@@ -145,7 +184,7 @@ class MainWindow(QMainWindow):
         print(a)
         t = time.localtime()
         current_time = time.strftime("%H:%M:%S", t)
-        print(current_time)
+        #print(current_time)
 
     def text_changed(self, b):  # s je text vybraneho prvku portu
         print(b)
@@ -170,7 +209,7 @@ class MainWindow(QMainWindow):
                 self.button.setChecked(False)
             else:
                 print("Port is open")
-                self.port.readyRead.connect(self.readFromPort)
+                self.port.readyRead.connect(self.cteni_dat)
                 self.button.setText("Connected")
                 self.button.setChecked(True)
                 self.port.setDataTerminalReady(True)
@@ -181,37 +220,6 @@ class MainWindow(QMainWindow):
             self.port.close()
             print("Port is closed")
 
-    def readFromPort(self):
-        data = self.port.readAll().data().decode('utf-8')
-        self.serialData.append(data)
-        print(data)
-        self.serialDataHex.append(data.encode('utf-8').hex())
-
-
-    def update_plot_data(self):
-        data = self.port.readLine()
-        if data:
-            data_str = data.data().decode().strip()
-            values = data_str.split(",")
-
-            if len(values) == 3:
-                try:
-                    ppm = float(values[0])
-
-                    self.x = self.x[1:]  # Remove the first x element.
-                    self.x.append(self.x[-1] + 1)  # Add a new value 1 higher than the last.
-
-                    self.y_ppm = self.y_ppm[1:]  # Remove the first ppm value.
-                    self.y_ppm.append(ppm)  # Add the new ppm value
-
-                    self.data_line_ppm.setData(self.x, self.y_ppm)
-
-                except ValueError:
-                    print("Invalid data format")
-            else:
-                print("Invalid data format")
-
-
     def baudRate(self):
         return int(self.baudRates.currentText())
         print(self.baudRates.currentText())
@@ -219,17 +227,6 @@ class MainWindow(QMainWindow):
     def portName(self):
         return self.portNames.currentText()
 
-    def return_pressed(self, i):
-        print(i)
-
-    def selection_changed(self, j):
-        print(j)
-
-    def text_changed(self, k):
-        print(k)
-
-    def text_edited(self, l):
-        print(l)
 
     def save_file(self):
         file_name = self.fileNameInput.text()
